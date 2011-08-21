59e4a2fefecf298997eb99f520348fa222f731ca
Eric Arneback <erkastina@hotmail.com>
2011-08-21 20:58:07 +0200

    Added simple command line "parsing" and file loading code.

--- code/tga/tga.c
+++ code/tga/tga.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "tga.h"
 
-int main()
+void loadTGA(const char * file)
 {
+    FILE * fp;
+
+    fp=fopen(file,"rb");
+
+    if(fp == NULL){
+	perror("An error occurred opening the file");
+	exit(1);
+    }
+
     
-    
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc == 1){
+	printf("No file was specified for loading");
+	return 1;
+    } else{
+	loadTGA(argv[1]);
+    }
     return 0;
 }
--- code/tga/tga.h
+++ code/tga/tga.h
@@ -1,7 +1,6 @@
 #ifndef _TGA_H_
 #define _TGA_H_
 
-
-
 #endif /* _TGA_H_ */
+
 
