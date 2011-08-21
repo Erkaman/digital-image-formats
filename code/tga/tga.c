e6537e331eeb4886bb209b9fb74e124785745ec3
Eric Arneback <erkastina@hotmail.com>
2011-08-21 22:25:47 +0200

    Created the struct describing the TGA file header and wrote code to load said file header into memory. The information from the loaded file header is then written to a file with an extension dmp for debugging purposes.

--- code/tga/tga.c
+++ code/tga/tga.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "tga.h"
-
-void loadTGA(const char * file)
-{
-    FILE * fp;
-
-    fp=fopen(file,"rb");
-
-    if(fp == NULL){
-	perror("An error occurred opening the file");
-	exit(1);
-    }
-
-    
-}
 
 int main(int argc, char *argv[])
 {
@@ -26,4 +13,54 @@ int main(int argc, char *argv[])
 	loadTGA(argv[1]);
     }
     return 0;
+}
+
+void loadTGA(char * file)
+{
+    FILE * in;
+    FILE * out;
+
+    in = fopen(file,"rb");
+
+    if(in == NULL){
+	perror("An error occurred opening the file");
+	exit(1);
+    }
+
+    TGAHeader tgah;
+
+    fread(&tgah,sizeof(TGAHeader),1,in);
+
+    /* Works because all tga files has a tga extension(I hope).*/
+    strip_extension(file);
+    strcat(file,".dmp");
+
+    out = fopen(file,"wb");
+
+    fprintf(out,"Id Length:%d\n",tgah.IDLength);
+    fprintf(out,"Color map type:%d\n",tgah.colorMapType);
+    fprintf(out,"Image type:%d\n",tgah.imageType);
+    fprintf(out,"Color map type:%d\n",tgah.colorMapStart);
+    fprintf(out,"Color map length:%d\n",tgah.colorMapLength);
+    fprintf(out,"Color map depth:%d\n",tgah.colorMapDepth);
+    fprintf(out,"X origin:%d\n",tgah.xOrigin);
+    fprintf(out,"Y origin:%d\n",tgah.yOrigin);
+    fprintf(out,"Width:%d\n",tgah.width);
+    fprintf(out,"Height:%d\n",tgah.height);
+    fprintf(out,"pixelDepth:%d\n",tgah.pixelDepth);
+    fprintf(out,"Image Descriptor:%d\n",tgah.imageDescriptor);
+
+}
+
+void strip_extension(char * fileName)
+{
+    char * extBeg;
+    int n;
+    extBeg = strrchr(fileName,'.');
+    if(extBeg == NULL);
+	//return extBeg;
+    else{
+	n = extBeg - fileName;
+	fileName[n] = '\0';
+    }
 }
--- code/tga/tga.h
+++ code/tga/tga.h
@@ -1,6 +1,34 @@
 #ifndef _TGA_H_
 #define _TGA_H_
+#include <stdint.h>
 
+/* A byte is an unsigned 8-bit integer in the TGA specification. */
+#define BYTE uint8_t
+
+/* A short is an unsigned 16-bit integer in the TGA specification. */
+#define SHORT uint16_t
+
+/* A short is an unsigned 16-bit integer in the TGA standard. */
+#define LONG uint32_t
+
+typedef struct
+{
+    BYTE IDLength;
+    BYTE colorMapType;
+    BYTE imageType;
+    SHORT colorMapStart;
+    SHORT colorMapLength;
+    BYTE colorMapDepth;
+    SHORT xOrigin;
+    SHORT yOrigin;
+    SHORT width;
+    SHORT height;
+    BYTE pixelDepth;
+    BYTE imageDescriptor;
+} TGAHeader;
+
+void loadTGA(char * file);
+
+/* strip the file extension of a filename. */
+void strip_extension(char * fileName);
 #endif /* _TGA_H_ */
-
-
