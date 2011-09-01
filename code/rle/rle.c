c8df9cc5a68a4d1a7ec48db6546c2178ea597cae
Eric Arneback <erkastina@hotmail.com>
2011-09-01 22:01:43 +0200

    Implemented the function which decodes rle.
    Wrote a simple command line parser and added support for decoding from
    the command line.

--- code/rle/rle.c
+++ code/rle/rle.c
@@ -4,6 +4,35 @@
 #include <string.h>
 
 
+void decode(char * inputfile,char * outputfile)
+{
+    FILE * in;
+    FILE * out;
+    char c,count;
+
+    in = fopen(inputfile,"rb");
+    out = fopen(outputfile,"wb");
+
+    assertFileOpened(in);
+    assertFileOpened(out);
+
+    count = -1;
+    while ((c = getc(in)) != EOF){
+        /* if the counter hasn't been assigned to yet. */
+        if(count == -1){
+            count = c;
+        }else{
+            for(; count > 0; --count){
+                putc(c,out);
+            }
+            count = -1;
+        }
+    }
+
+    fclose(in);
+    fclose(out);
+}
+
 void encode(char * inputfile,char * outputfile)
 {
     FILE * in;
@@ -46,12 +75,36 @@ void encode(char * inputfile,char * outputfile)
 
 int main(int argc, char *argv[])
 {
-    char * encodedFile;
-    if(argc != 1){
-	encodedFile = changeExtension(argv[1],"rle");
-        encode(argv[1],encodedFile);
-	free(encodedFile);
+    char * result;
+    int decompress = 0;
+
+    if(argc == 1){
+        printf("No arguments specified\n");
+        return 1;
     }
+
+    /* parse command line arguments */
+
+    ++argv;
+
+    while((*argv)[0] == '-'){
+        switch((*argv)[1]){
+        case 'd':
+            decompress = 1;
+            break;
+        }
+        ++argv;
+    }
+
+    if(decompress){
+        result = changeExtension(*argv,"dec");
+        decode(*argv,result);
+    }else{
+        result = changeExtension(*argv,"rle");
+        encode(*argv,result);
+    }
+
+    free(result);
 
     return 0;
 }
