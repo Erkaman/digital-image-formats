0a86fa084099d9256e8f84b5ce7eabe717ed0da9
Eric Arneback <erkastina@hotmail.com>
2011-10-02 20:43:51 +0200

    Got a first version of the compression algorithm LZW working.

--- code/lzw/lzw.c
+++ code/lzw/lzw.c
@@ -5,6 +5,25 @@
 
 void printHelp(void);
 void lzw_compress(FILE * in,FILE * our);
+void outputCode(FILE * out,unsigned int code);
+
+void outputStringCode(FILE * out);
+
+#define BITS 12
+#define SIZE 4096 /* or sumthin */
+#define MAX_VALUE (1 << BITS) - 1 /* Note that MS-DOS machines need to   */
+#define MAX_CODE MAX_VALUE - 1    /* compile their code in large model if*/
+
+char ** dictionary;
+
+char stringCodeStack[40000];
+int stackp;
+
+int find_code(unsigned int charCode);
+
+char * constructNewDictionaryEntry(unsigned int charCode);
+void addToDictionary (int index,unsigned int charCode);
+void printStringCode(unsigned int stringCode);
 
 int main(int argc, char *argv[])
 {
@@ -13,13 +32,16 @@ int main(int argc, char *argv[])
     FILE * out;
     char * outFile;
     char * inFile;
+    int i;
+
+    out = fopen("codes","wb");
 
     if(argc == 1){
         printf("No file was specified.\n");
         printf("Try --help for more information.\n");
     } else{
 
-	++argv;
+        ++argv;
 
         if(!strcmp("--help",*argv)){
             printHelp();
@@ -30,27 +52,31 @@ int main(int argc, char *argv[])
             ++argv;
         }
 
+        dictionary = (char **)malloc(sizeof(char *) * SIZE);
 
         if(decompress){
 
         }else{
-	    inFile = *argv;
+            inFile = *argv;
             in = fopen(inFile,"rb");
             assertFileOpened(in);
 
-	    outFile = (char *)malloc(strlen(*argv) + 4);
-	    strcpy(outFile,*argv);
-	    strcat(outFile,".lzw");
+            outFile = (char *)malloc(strlen(*argv) + 4);
+            strcpy(outFile,*argv);
+            strcat(outFile,".lzw");
             out = fopen(outFile,"wb");
-	    free(outFile);
+            free(outFile);
             assertFileOpened(out);
 
-	    lzw_compress(in,out);
-	    fclose(in);
-	    fclose(out);
-        }
+            lzw_compress(in,out);
+            fclose(in);
+            fclose(out);
 
-        /* load*/
+            printf("Dictionary\n");
+            for(i = 0;i < 13; ++i){
+                printf("%d : %s\n",i + 256,dictionary[i]);
+            }
+        }
     }
 
     return 0;
@@ -68,6 +94,138 @@ void printHelp(void)
 
 void lzw_compress(FILE * in,FILE * out)
 {
-    in = in;
-    out = out;
+/*    unsigned int stringCode; */
+    unsigned int charCode;
+    unsigned int i;
+    int index;
+    int formerIndex;
+
+    for(i = 0; i < SIZE; ++i){
+        dictionary[i] = NULL;
+    }
+
+    formerIndex = -1;
+
+    stringCodeStack[stackp++] = getc(in);
+/*    stringCode = getc(in); */
+
+    charCode = getc(in);
+    while (charCode != (unsigned int)EOF){
+
+        index = find_code(charCode);
+
+        if(dictionary[index] != NULL){
+            stringCodeStack[stackp++] = charCode;
+            formerIndex = index;
+        }else{
+
+            if(formerIndex != -1){
+                outputCode(out,formerIndex + 256);
+            } else{
+                outputStringCode(out);
+            }
+            formerIndex = -1;
+/*            outputCode(out,stringCode); */
+
+            /* if less than the maximum size */
+            if(1){
+                addToDictionary(index,charCode);
+            }
+
+	    stackp = 0;
+
+            stringCodeStack[stackp++] = charCode;
+/*            stringCode = charCode;*/
+        }
+
+        charCode = getc(in);
+    }
+
+/*    outputStringCode(out); */
+    if(formerIndex != -1){
+        outputCode(out,formerIndex + 256);
+    } else{
+        outputStringCode(out);
+    }
+    outputCode(out,0);
 }
+
+void outputStringCode(FILE * out)
+{
+    int length = stackp;
+    int i;
+
+    for(i = 0; i < length; ++i){
+        outputCode(out,stringCodeStack[i]);
+    }
+}
+
+void outputCode(FILE * out,unsigned int code)
+{
+    static int output_bit_count=0;
+    static unsigned long output_bit_buffer=0L;
+
+/*    if(dictionary[code] != NULL){
+      code += 256;
+      }*/
+
+    printf("%d=%c\n",code,(char)(code));
+
+    output_bit_buffer |= (unsigned long) code << (32 - BITS- output_bit_count);
+    output_bit_count += BITS;
+    while (output_bit_count >= 8)
+    {
+        putc(output_bit_buffer >> 24,out);
+        output_bit_buffer <<= 8;
+        output_bit_count -= 8;
+    }
+}
+
+int find_code(unsigned int charCode)
+{
+    int index;
+    char * str;
+
+    str = constructNewDictionaryEntry(charCode);
+
+    for(index = 0;index < SIZE; ++index){
+        if(dictionary[index] == NULL){
+            break;
+        }
+
+        if(!strcmp(str,dictionary[index])){
+            break;
+        }
+    }
+    free(str);
+
+    return index;
+}
+
+char * constructNewDictionaryEntry(unsigned int charCode)
+{
+    char * str;
+    int i;
+    /* string + character */
+
+    str = (char * )malloc((stackp + 1) * sizeof(char));
+
+    for(i = 0; i < stackp; ++i){
+        str[i] = stringCodeStack[i];
+    }
+
+    str[i++] = charCode;
+    str[i] = '\0';
+
+    return str;
+}
+
+void addToDictionary (int index,unsigned int charCode)
+{
+    char * str;
+
+    str = constructNewDictionaryEntry(charCode);
+
+    dictionary[index] = str;
+}
+
