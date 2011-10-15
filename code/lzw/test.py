d0568e28f8ae2fb7b65d5632ec1cbca7893c1e29
Eric Arneback <erkastina@hotmail.com>
2011-10-15 14:56:18 +0200

    Wrote a python script which tests the compression ratio of the LZW program for different code sizes and different files from the Canterbury Corpus.
    After it has tested all the different files and codes sizes, it outputs all the
    different compression ratios as latex table to a file.

---
+++ code/lzw/test.py
@@ -0,0 +1,67 @@
+import os
+
+def calcCompessionRatio(uncompressedSize,compressedSize):
+    """ Calculates the compression ratio of two file sizes.
+    """
+    return round((float(compressedSize) / uncompressedSize) * 100,1)
+
+testDirectory= "../testfiles/"
+
+out = open('table.tex', 'w')
+
+newTableLine = """\\\\
+"""
+
+tableFirst ="""\\begin{table}
+  \\scriptsize
+  \\centering
+  \\noindent\\makebox[\\textwidth]{%
+  \\begin{tabular}{"""
+
+out.write(tableFirst)
+
+out.write('l' * (2 + (15 - 9 + 1)))
+
+out.write("""}
+    \\toprule
+    File & Original size & """)
+
+for codeSize in range(9,16):
+    out.write(str(codeSize) + \
+              ( '-bit codes(\%) & ' if codeSize != 15 else '-bit codes(\%) ' + newTableLine ))
+
+tableEnd = """    \\bottomrule
+  \\end{tabular}}
+  \\caption{LZW test results. The different percentages the represent the respective compression ratios of that code size.}
+  \\label{tab:corp-files}
+\end{table}"""
+
+
+
+dirList = os.listdir(testDirectory)
+
+for fileName in dirList:
+
+    if fileName[-4:] != '.lzw':
+        out.write(fileName + ' & ')
+
+        fileName = os.path.join(testDirectory,fileName)
+        uncompressedSize = os.path.getsize(fileName)
+        out.write(str(uncompressedSize) + ' & ')
+
+        for codeSize in range(9,16):
+
+            command = './lzw ' + '-cs=' + str(codeSize) + ' ' + fileName
+            os.system(command)
+
+            compressedSize = os.path.getsize(fileName + '.lzw')
+            #            print "Compressed size: " + str(compressedSize)
+
+            out.write(str(calcCompessionRatio(uncompressedSize,compressedSize)) + \
+                      (' & ' if codeSize != 15 else newTableLine))
+            # delete the outputted file.
+            os.remove(fileName + '.lzw')
+
+out.write(tableEnd)
+
+out.close()
