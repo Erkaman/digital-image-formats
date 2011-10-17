456dcac650cb1f025d3f20616f8364db07c18d23
Eric Arneback <erkastina@hotmail.com>
2011-10-17 17:36:19 +0200

    Now the GIF loader is able to load and print out the header information of a GIF file.

--- code/gif/gif.c
+++ code/gif/gif.c
@@ -12,8 +12,16 @@ int * localColorTable;
 /* use a flag to specify whether the current image has local color table or not,
    else use the global color table */
 
+GIFHeader header;
+
 void printHelp(void);
 void loadGIF(char * file);
+UNSIGNED readUnsigned(FILE * fp);
+
+void readImageInfo(FILE * in);
+void printImageInfo(FILE * out);
+
+void loadHeader(FILE * in);
 
 int main(int argc, char *argv[])
 {
@@ -27,6 +35,13 @@ int main(int argc, char *argv[])
     loadGIF(argv[1]);
 
     return 0;
+}
+
+UNSIGNED readUnsigned(FILE * fp)
+{
+    UNSIGNED s;
+    fread(&s,sizeof(UNSIGNED),1,fp);
+    return s;
 }
 
 void printHelp(void)
@@ -50,10 +65,35 @@ void loadGIF(char * file)
     assertFileOpened(out);
     free(file);
 
+    /* read the image info; things like color table and headers */
+    readImageInfo(in);
+
     fprintf(out,"Image info:\n");
+
+    printImageInfo(out);
 
     fprintf(out,"Color data:\n");
 
     fclose(in);
     fclose(out);
+}
+
+void readImageInfo(FILE * in)
+{
+    loadHeader(in);
+}
+
+void loadHeader(FILE * in)
+{
+    readStr(in,3,header.signature);
+    header.signature[3] = '\0';
+
+    readStr(in,3,header.version);
+    header.version[3] = '\0';
+}
+
+void printImageInfo(FILE * out)
+{
+    fprintf(out,"Signature:%s\n",header.signature);
+    fprintf(out,"Version:%s\n",header.version);
 }
