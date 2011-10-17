c0b7e70794c0337254669dbfd23ec344223306ee
Eric Arneback <erkastina@hotmail.com>
2011-10-17 17:18:23 +0200

    Write the code in the GIF program that does the little file handling that the program needs.

--- code/gif/gif.c
+++ code/gif/gif.c
@@ -1,10 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "gif.h"
+
+/* Used by all images that do not have a local color table */
+int * globalColorTable;
+
+/* used by the graphics that specifies their own local color tables */
+int * localColorTable;
+
+/* use a flag to specify whether the current image has local color table or not,
+   else use the global color table */
+
+void printHelp(void);
+void loadGIF(char * file);
 
 int main(int argc, char *argv[])
 {
-    argc = argc;
-    argv = argv;
-    printf("hello gif\n");
+    if(argc == 1){
+        printf("No file was specified for loading\n");
+        printf("Try --help for more information.\n");
+        return 1;
+    } else
+        if(!strcmp("--help",argv[1]))
+            printHelp();
+    loadGIF(argv[1]);
+
     return 0;
+}
+
+void printHelp(void)
+{
+    printf("Usage: gif IN\n");
+    printf("Dump the information and color data of a GIF file.\n");
+    printf("  --help\tDisplay this help message.\n");
+
+}
+
+void loadGIF(char * file)
+{
+    FILE * in;
+    FILE * out;
+
+    in = fopen(file,"rb");
+    assertFileOpened(in);
+
+    file = changeExtension(file,"dmp");
+    out = fopen(file,"wb");
+    assertFileOpened(out);
+    free(file);
+
+    fprintf(out,"Image info:\n");
+
+    fprintf(out,"Color data:\n");
+
+    fclose(in);
+    fclose(out);
 }
