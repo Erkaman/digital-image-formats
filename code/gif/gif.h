e200bded6296f89986c1a9648e5408fb6d0682f0
Eric Arneback <erkastina@hotmail.com>
2011-10-18 21:09:47 +0200

    Put all of the function prototypes, structs AND globals into the gif.h header file.

--- code/gif/gif.h
+++ code/gif/gif.h
@@ -10,6 +10,18 @@
 
 #define FLAG int
 
+/* the color depth of the colors in a GIF is always 24 */
+#define COLOR_DEPTH 24
+#define TRAILER 0x3b
+#define EXTENSION_INTRODUCER 0x21
+#define GRAPHIC_CONTROL_LABEL 0xf9
+#define COMMENT_LABEL 0xfe
+#define APPLICATION_EXTENSION_LABEL 0xff
+#define PLAIN_TEXT_LABEL 0x01
+#define IMAGE_SEPARATOR 0x2c
+
+#define DEBUG 1
+
 typedef struct{
     char signature[4];
     char version[4];
@@ -64,5 +76,77 @@ typedef struct{
     BYTE blockTerminator;
 
 } GIFGraphicControl;
+
+typedef struct {
+    unsigned int stringCode;
+    unsigned int characterCode;
+} tableEntry;
+
+tableEntry * compressionTable;
+
+
+/* Used by all images that do not have a local color table */
+unsigned long * globalColorTable;
+
+/* used by the graphics that specifies their own local color tables */
+int * localColorTable;
+
+/* use a flag to specify whether the current image has local color table or not,
+   else use the global color table */
+
+unsigned int nextCode;
+
+char stringCodeStack[40000];
+int stackp;
+
+unsigned int ClearCode;
+unsigned int EndCode;
+
+
+GIFHeader header;
+GIFLogicalScreenDescriptor logicalScreenDescriptor;
+GIFImageDescriptor imageDescriptor;
+
+GIFGraphicControl graphicControl;
+
+void debugPrint(const char * format, ...);
+
+void printHelp(void);
+void loadGIF(char * file);
+
+UNSIGNED readUnsigned(FILE * fp);
+void readImageInfo(FILE * in);
+
+void loadImageData(FILE * in,FILE * out);
+void loadExtension(FILE * in,FILE * out);
+
+char printString(FILE * out);
+
+void printImageInfo(FILE * out);
+void printSignature(FILE * out);
+void printLogicalScreenDescriptor(FILE * out);
+void printGlobalColorTable(FILE * out);
+void printTableColor(int index,unsigned long * colorTable,FILE * out);
+
+void printColor(int index,unsigned long * colorTable,FILE * out);
+
+void loadHeader(FILE * in);
+void loadLogicalScreenDescriptor(FILE * in);
+void loadGlobalColorTable(FILE * in);
+
+void loadImageDescriptor(FILE * in);
+void printImageDescriptor(FILE * out);
+
+void loadGraphicControl(FILE * in);
+void printGraphicControl(FILE * out);
+
+void loadImageColorData(FILE * in,FILE * out);
+
+void resetCompressionTable(void);
+
+unsigned int inputCode(int codeSize,FILE *input);
+
+void translateCode(unsigned int newCode);
+
 
 #endif /* _GIF_H_ */
