168906ee5f2d2f1525c701024f4d6ef8a49783c5
Eric Arneback <erkastina@hotmail.com>
2011-11-26 11:53:07 +0100

    Implemented support for loading and dumping PNG signatures.

--- code/png/png.c
+++ code/png/png.c
@@ -2,6 +2,86 @@
 
 void dumpPNG(FILE * in, FILE * out)
 {
+    PNG_Image image;
+
+    image = loadPNG(in);
+    writePNG(image,out);
+}
+
+PNG_Signature loadPNG_Signature(FILE * in)
+{
+    PNG_Signature signature;
+
+    signature.signatureData = allocateDataContainer(8);
+
+    fread(signature.signatureData.data, sizeof(BYTE), 8, in);
+    signature.signatureData.size = 8;
+
+    return signature;
+}
+
+PNG_Image loadPNG(FILE * in)
+{
+    PNG_Image image;
+
+    image.signature = loadPNG_Signature(in);
+    /* TODO: Validate signature. */
+
+    image.header = loadPNG_ImageHeader(in);
+
+    return image;
+}
+
+void writePNG(PNG_Image image, FILE * out)
+{
+    writePNG_Signature(image.signature, out);
+}
+
+void writePNG_Signature(PNG_Signature signature, FILE * out)
+{
+    unsigned long i;
+
+    fprintf(out, "PNG Signature:\n");
+    for(i = 0; i < signature.signatureData.size; ++i){
+	if(i != (signature.signatureData.size - 1)){
+	    fprintf(out, "%d ", signature.signatureData.data[i]);
+	} else{
+	    fprintf(out, "%d\n", signature.signatureData.data[i]);
+	}
+    }
+}
+
+PNG_Chunk loadPNG_Chunk(FILE * in)
+{
+    PNG_Chunk chunk;
+
+    verbosePrint("Loading chunk...\n");
+
+    fread(&chunk.length, sizeof(uint32_t), 1, in);
+    chunk.length = htonl(chunk.length);
+
+    verbosePrint("Chunk length: %ld\n", chunk.length);
+
+    fread(chunk.chunkType, sizeof(char), 4, in);
+    chunk.chunkType[4] = '\0';
+    verbosePrint("Chunk Type: %s\n", chunk.chunkType);
+
+    fread(chunk.chunkData.data, sizeof(BYTE), chunk.length, in);
+    signature.signatureData.size = chunk.length;
+
+    return chunk;
+}
+
+PNG_ImageHeader loadPNG_ImageHeader(FILE * in)
+{
+    PNG_ImageHeader header;
+    PNG_Chunk headerChunk;
+
+    headerChunk = loadPNG_Chunk(in);
+
+    headerChunk = headerChunk;
+    header = header;
     in = in;
-    out = out; 
+
+    return header;
 }
--- code/png/png.h
+++ code/png/png.h
@@ -2,15 +2,52 @@
 #define _PNG_H_
 
 #include "../common.h"
+#include <stdint.h>
 
 typedef struct {
     DataContainer signatureData;
 } PNG_Signature;
 
+
+typedef struct {
+    uint32_t length;
+    char chunkType[5];
+    DataContainer chunkData;
+    uint32_t CRC;
+} PNG_Chunk;
+
+typedef struct {
+    uint32_t width;
+    uint32_t height;
+
+    BYTE bitDepth;
+    BYTE colorType;
+
+    BYTE compressionMethod;
+    BYTE filterMethod;
+    BYTE interlaceMethod;
+
+} PNG_ImageHeader;
+
 typedef struct {
     PNG_Signature signature;
+
+    PNG_ImageHeader header;
+
 } PNG_Image;
+
 
 void dumpPNG(FILE * in, FILE * out);
 
+PNG_Image loadPNG(FILE * in);
+PNG_Signature loadPNG_Signature(FILE * in);
+PNG_ImageHeader loadPNG_ImageHeader(FILE * in);
+PNG_Chunk loadPNG_Chunk(FILE * in);
+
+void writePNG(PNG_Image image, FILE * out);
+
+void writePNG_Signature(PNG_Signature signature, FILE * out);
+
+
 #endif /* _PNG_H_ */
+
