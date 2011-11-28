b13f9392ea6a3e7db33ca3eacc9035c4f9869657
Eric Arneback <erkastina@hotmail.com>
2011-11-28 22:59:50 +0100

    Created all the structs and constants associated with the PNG format.

--- code/png/png.c
+++ code/png/png.c
@@ -1,4 +1,7 @@
 #include "png.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 void dumpPNG(FILE * in, FILE * out)
 {
@@ -8,9 +11,9 @@ void dumpPNG(FILE * in, FILE * out)
     writePNG(image,out);
 }
 
-PNG_Signature loadPNG_Signature(FILE * in)
+Signature loadSignature(FILE * in)
 {
-    PNG_Signature signature;
+    Signature signature;
 
     signature.signatureData = allocateDataContainer(8);
 
@@ -23,65 +26,147 @@ PNG_Signature loadPNG_Signature(FILE * in)
 PNG_Image loadPNG(FILE * in)
 {
     PNG_Image image;
+    Chunk chunk;
 
-    image.signature = loadPNG_Signature(in);
+    image.signature = loadSignature(in);
     /* TODO: Validate signature. */
 
-    image.header = loadPNG_ImageHeader(in);
+    image.header = loadImageHeader(in);
+
+    while(1){
+
+	chunk = loadChunk(in);
+
+	if(!strcmp(chunk.chunkType, IEND)){
+	    break;
+	} else if(!strcmp(chunk.chunkType, IDAT)){
+	    /* read the rest of the IDATs */
+	} else {
+	    if(!isCriticalChunk(chunk)){
+		printWarning("Unknown ancillary chunk %s found, skipping chunk.",
+		    chunk.chunkType);
+	    } else{
+		printError("Unknown critcal chunk %s found.", chunk.chunkType);
+		exit(1);
+	    }
+	}
+
+    }
 
     return image;
 }
 
 void writePNG(PNG_Image image, FILE * out)
 {
-    writePNG_Signature(image.signature, out);
+    writeSignature(image.signature, out);
 }
 
-void writePNG_Signature(PNG_Signature signature, FILE * out)
+void writeSignature(Signature signature, FILE * out)
 {
     unsigned long i;
 
     fprintf(out, "PNG Signature:\n");
     for(i = 0; i < signature.signatureData.size; ++i){
-	if(i != (signature.signatureData.size - 1)){
-	    fprintf(out, "%d ", signature.signatureData.data[i]);
-	} else{
-	    fprintf(out, "%d\n", signature.signatureData.data[i]);
-	}
+        if(i != (signature.signatureData.size - 1)){
+            fprintf(out, "%d ", signature.signatureData.data[i]);
+        } else{
+            fprintf(out, "%d\n", signature.signatureData.data[i]);
+        }
     }
 }
 
-PNG_Chunk loadPNG_Chunk(FILE * in)
+Chunk loadChunk(FILE * in)
 {
-    PNG_Chunk chunk;
+    Chunk chunk;
 
     verbosePrint("Loading chunk...\n");
 
     fread(&chunk.length, sizeof(uint32_t), 1, in);
     chunk.length = htonl(chunk.length);
 
-    verbosePrint("Chunk length: %ld\n", chunk.length);
+    verbosePrint("Chunk length: % ld\n", chunk.length);
 
     fread(chunk.chunkType, sizeof(char), 4, in);
     chunk.chunkType[4] = '\0';
     verbosePrint("Chunk Type: %s\n", chunk.chunkType);
 
+    /* TODO: Remember to free this memory! */
+    chunk.chunkData = allocateDataContainer(chunk.length);
     fread(chunk.chunkData.data, sizeof(BYTE), chunk.length, in);
-    signature.signatureData.size = chunk.length;
+
+    fread(&chunk.CRC, sizeof(uint32_t), 1, in);
+    chunk.CRC = htonl(chunk.CRC);
+    verbosePrint("Chunk CRC: %ld\n", chunk.CRC);
+
+    validateCRC(chunk);
+
+    /* Validate CRC. */
 
     return chunk;
 }
 
-PNG_ImageHeader loadPNG_ImageHeader(FILE * in)
+void validateCRC(Chunk chunk)
 {
-    PNG_ImageHeader header;
-    PNG_Chunk headerChunk;
+    DataContainer checkData;
+    unsigned long i;
+    unsigned long calcCrc32;
 
-    headerChunk = loadPNG_Chunk(in);
+    /* Get the data to be validated: the chunk type and the chunk data */
+
+    /* The chunk length bytes are included in the calculation. */
+    checkData = allocateDataContainer(chunk.length + 4);
+
+    for(i = 0; i < 4; ++i)
+	checkData.data[i] = chunk.chunkType[i];
+
+    for(i = i; i < (chunk.length + 4); ++i)
+	checkData.data[i] = chunk.chunkData.data[i-4];
+
+    verbosePrint("CRC data:\n");
+    printData(checkData);
+
+    calcCrc32 = crc32(checkData);
+
+/*    verbosePrint("crc32:%d\n",); */
+
+    /* */
+}
+
+int isCriticalChunk(Chunk chunk)
+{
+    return isupper((BYTE)chunk.chunkType[0]);
+}
+
+ImageHeader loadImageHeader(FILE * in)
+{
+    ImageHeader header;
+    Chunk headerChunk;
+
+    headerChunk = loadChunk(in);
 
     headerChunk = headerChunk;
     header = header;
     in = in;
 
     return header;
+}
+
+#define CRC32_POLY 0xEDB88320
+
+unsigned int crc32(DataContainer data){
+
+    unsigned long reminder = 0xFFFFFFFF; /* standard initial value in CRC32 */
+    unsigned long i;
+    unsigned long bit;
+
+    for(i = 0; i < data.size; ++i){
+        reminder ^= data.data[i]; /* must be zero extended */
+        for(bit = 0; bit < 8; bit++)
+            if(reminder & 0x01)
+                reminder = (reminder >> 1) ^ CRC32_POLY;
+            else
+                reminder >>= 1;
+    }
+
+    return reminder ^ 0xFFFFFFFF;
 }
--- code/png/png.h
+++ code/png/png.h
@@ -6,15 +6,14 @@
 
 typedef struct {
     DataContainer signatureData;
-} PNG_Signature;
-
+} Signature;
 
 typedef struct {
     uint32_t length;
     char chunkType[5];
     DataContainer chunkData;
     uint32_t CRC;
-} PNG_Chunk;
+} Chunk;
 
 typedef struct {
     uint32_t width;
@@ -27,27 +26,281 @@ typedef struct {
     BYTE filterMethod;
     BYTE interlaceMethod;
 
-} PNG_ImageHeader;
+} ImageHeader;
 
 typedef struct {
-    PNG_Signature signature;
+    uint32_t whitePointX;
+    uint32_t whitePointY;
 
-    PNG_ImageHeader header;
+    uint32_t RedX;
+    uint32_t RedY;
+
+    uint32_t GreenX;
+    uint32_t GreenY;
+
+    uint32_t BlueX;
+    uint32_t BlueY;
+
+} PrimaryChromaticities;
+
+typedef struct{
+    char profileName[80];
+
+    /* I'm not really sure how the data is stored here.
+
+     See http://www.w3.org/TR/PNG/#11iCCP
+     and
+    http://www.color.org/ICC1V42.pdf*/
+    DataContainer profile;
+} ICC_Profile;
+
+/* How this structures should be interpreted depends on the color type used. */
+typedef struct {
+    BYTE significantGreyscaleBits;
+
+    BYTE significantRedBits;
+    BYTE significantGreenBits;
+    BYTE significantBlueBits;
+
+    BYTE significantAlphaBits;
+
+} SignificantBits;
+
+typedef struct{
+    char keyword[80];
+
+    char * str;
+} TextualData;
+
+typedef struct{
+    char keyword[80];
+
+    BYTE compressionMethod;
+
+    char * str;
+} CompressedTextualData;
+
+typedef struct {
+    char keyword[80];
+
+    /* Indicates whether the data is compressed. */
+    BYTE compressionFlag;
+    /* Indicates the compression method */
+    BYTE compressionMethod;
+
+    char * languageTag;
+
+    char * translatedKeyword;
+
+    /* Only the text field may be compressed */
+    char * text;
+
+} InternationalTextualData;
+
+typedef struct {
+
+    TextualData * pairsList;
+
+    InternationalTextualData * internationalText;
+
+} textDataList;
+
+typedef struct {
+    unsigned long R;
+    unsigned long G;
+    unsigned long B;
+} RGB;
+
+typedef struct {
+    unsigned long R;
+    unsigned long G;
+    unsigned long B;
+    unsigned long A;
+} RGBA;
+
+typedef union {
+
+    RGB rgb;
+
+    RGBA rgba;
+
+    unsigned long index;
+
+    unsigned long greyscale;
+
+} Color;
+
+typedef struct {
+
+    Color * colors;
+
+    unsigned long size;
+
+} ColorData;
+
+
+typedef struct {
+    ColorData * palette;
+
+    unsigned long size;
+} Palette;
+
+typedef struct {
+    Palette * pallettes;
+
+    unsigned long size;
+} PaletteList;
+
+
+typedef struct {
+    uint32_t x;
+    uint32_t y;
+
+    BYTE unitSpecifier;
+
+} PixelDimensions;
+
+typedef struct {
+    uint16_t year;
+
+    BYTE month;
+    BYTE day;
+    BYTE hour;
+    BYTE minute;
+    BYTE second;
+} TimeStamp;
+
+typedef struct {
+    Signature signature;
+
+    ImageHeader header;
+
+    /* Put in the proper order. */
+    /* The compressed datastream is then the concatenation of the
+     * contents of the data fields of all the IDAT chunks. */
+    ColorData colorData;
+
+    Palette palette;
+
+    /* Suggested palettes */
+
+    /* ICC Profile. */
+    ICC_Profile * profile;
+
+    PrimaryChromaticities * chromaticities;
+
+    /* The gamma chunk contains this and only this value. */
+    uint32_t * imageGamma;
+
+    SignificantBits * significantBits;
+
+    BYTE * renderingIntent;
+
+    Color * backgroundColor;
+
+    uint16_t * imageHistogram;
+
+    PixelDimensions * pixelDimensions;
+
+    PaletteList * suggestedPalettes;
+
+    TimeStamp * timeStamp;
 
 } PNG_Image;
 
+/* Header chunk. */
+#define IHDR "IHDR"
+
+/* Palette */
+#define PLTE "PLTE"
+
+/* Image data chunk. */
+#define IDAT "IDAT"
+
+/* Ending chunk. */
+#define IEND "IEND"
+
+/* Ending chunk. */
+#define tRNS  "tRNS"
+
+#define cHRM  "cHRM"
+
+#define gAMA "gAMA"
+
+#define iCCP "iCCP"
+
+#define sBIT "sBIT"
+
+#define sRGB  "sRGB"
+
+#define tEXt "tEXt"
+
+#define zTXt "zTXt"
+
+#define iTXt "iTXt"
+
+#define bKGD "bKGD"
+
+#define hIST "hIST"
+
+#define pHYs "pHYs"
+
+#define sPLT "sPLT "
+
+#define tIME "tIME"
+
+
+#define GREYSCALE_COLOR 0
+#define TRUECOLOR_COLOR 2
+#define INDEXED_COLOR 3
+#define GREYSCALE_ALPHA_COLOR 4
+#define TRUECOLOR_ALPHA_COLOR 6
+
+/* The only allowed compression method. */
+#define DEFLATE_COMPRESSION_METHOD 0
+
+/* The only allowed filter method. */
+#define FILTER_METHOD_0 0
+
+/* Filter types: */
+
+#define NO_FILTER 0
+#define SUB_FILTER 1
+#define UP_FILTER 2
+#define AVERAGE_FILTER 3
+#define PAETH_FILTER 4
+
+/* Interlacing types. */
+
+#define NO_INTERLACE 0
+#define ADAM7_INTERLACE 1
+
+#define PERCEPTUAL_RENDERING_INTENT 0
+#define RELATIVE_COLORIMETRIC_RENDERING_INTENT 1
+#define SATURATION_RENDERING_INTENT 2
+#define ABSOLUTE_COLORIMETRIC_RENDERING_INTENT 3
+
+/* Units specifiers. Used in the pHYs chunk. */
+
+#define UNKNOWN_UNIT 0
+#define METRE_UNIT 1
 
 void dumpPNG(FILE * in, FILE * out);
 
 PNG_Image loadPNG(FILE * in);
-PNG_Signature loadPNG_Signature(FILE * in);
-PNG_ImageHeader loadPNG_ImageHeader(FILE * in);
-PNG_Chunk loadPNG_Chunk(FILE * in);
+Signature loadSignature(FILE * in);
+ImageHeader loadImageHeader(FILE * in);
+Chunk loadChunk(FILE * in);
 
 void writePNG(PNG_Image image, FILE * out);
 
-void writePNG_Signature(PNG_Signature signature, FILE * out);
+void writeSignature(Signature signature, FILE * out);
 
+unsigned int crc32(DataContainer data);
+
+void validateCRC(Chunk chunk);
+
+int isCriticalChunk(Chunk chunk);
 
 #endif /* _PNG_H_ */
 
