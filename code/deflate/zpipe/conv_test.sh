e1194a3b68d6300ee98a0349d3f0a015c1d89bf4
Eric Arneback <erkastina@hotmail.com>
2012-04-01 13:41:44 +0200

    Wrote a section on the performance problems of LZ77.

--- code/deflate/zpipe/conv_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BASE="$( cd -P "$( dirname "$0" )" && pwd )"
-cmd="${BASE}/a ${BASE}/test ${BASE}/test.zlib"
-$cmd
-cp "${BASE}/test.zlib" "/cygdrive/c/Users/Eric/Dropbox/school_project/code/deflate/test.zlib"
-
